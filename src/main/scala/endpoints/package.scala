4ace7fc070cb3249281cd188642775eff1f83d15
David Pratt <dpratt747@gmail.com>
2025-01-26 17:01:55 +0000

    An example of cask/upickle with scala futures

--- src/main/scala/endpoints/package.scala
+++ src/main/scala/endpoints/package.scala
@@ -1,7 +1,11 @@
 
+import cask.Response
 import domain.*
 import ujson.*
 import upickle.default.{write as uWrite, *}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 package object endpoints {
   private def convertToUserId(userId: ujson.Value): Either[String, UserId] = {
@@ -21,4 +25,16 @@ package object endpoints {
   private def jsonify[A: ReadWriter](caseClass: A): ujson.Obj = {
     ujson.read(uWrite(caseClass)).obj
   }
+
+  // can match on the type of the exception to return a different error message - status code
+  private val returnInternalServerErrorOnFailure: PartialFunction[Throwable, Response[ujson.Obj]] = {
+    case exception => cask.Response(jsonify(JsonResponse(s"Error occurred [$exception]")), 500)
+  }
+
+  extension (future: Future[Response[ujson.Obj]])
+    def mapFutureFailure(handleFailure: PartialFunction[Throwable, Response[ujson.Obj]])(implicit ec: ExecutionContext): Future[Response[ujson.Obj]] = {
+      future.recover(handleFailure)
+    }
+    def awaitResult(): Response[Obj] =
+      Await.result(future, Duration.Inf)
 }
--- src/main/scala/program/UserProgram.scala
+++ src/main/scala/program/UserProgram.scala
@@ -4,21 +4,22 @@ import db.DatabaseAlg
 import domain.{User, UserId}
 
 import scala.collection.mutable
+import scala.concurrent.Future
 
 trait UserProgramAlg {
-  def createNewUser(user: User): Unit
+  def createNewUser(user: User): Future[Unit]
 
-  def getAllUsers: scala.collection.mutable.Map[UserId, User]
+  def getAllUsers: Future[scala.collection.mutable.Map[UserId, User]]
 
-  def getUser(userId: UserId): Option[User]
+  def getUser(userId: UserId): Future[Option[User]]
 }
 
 private class UserProgram(database: DatabaseAlg) extends UserProgramAlg {
-  override def createNewUser(user: User): Unit = database.insertUser(user)
+  override def createNewUser(user: User): Future[Unit] = database.insertUser(user)
 
-  override def getAllUsers: mutable.Map[UserId, User] = database.getAllUsers
+  override def getAllUsers: Future[mutable.Map[UserId, User]] = database.getAllUsers
 
-  override def getUser(userId: UserId): Option[User] = database.getUser(userId)
+  override def getUser(userId: UserId): Future[Option[User]] = database.getUser(userId)
 }
 
 object UserProgram {
